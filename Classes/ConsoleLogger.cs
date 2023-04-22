29419af1710a320ffe309d501537fe00351643ec
Vadim Chuskov <vchuskov@tula.co>
2023-04-23 01:32:37 +0200

    init

---
+++ Classes/ConsoleLogger.cs
@@ -0,0 +1,25 @@
+﻿namespace ParserAliexpressOrderHistoryHar.Classes;
+
+public class ConsoleLogger
+{
+    public void Log(string text)
+    {
+        Console.WriteLine($"{DateTime.Now.TimeOfDay}: {text}");
+    }
+
+    public void Error(string text)
+    {
+        var previousColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{DateTime.Now.TimeOfDay}: {text}");
+        Console.ForegroundColor = previousColor;
+    }
+
+    public void GreenRaw(string text)
+    {
+        var previousColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(text);
+        Console.ForegroundColor = previousColor;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using ParserAliexpressOrderHistoryHar.Classes;
+
+const string harFilePath = @"c:\Hobby\ParserAliexpressOrderHistoryHar\www.aliexpress_full.com.har";
+
+var logger = new ConsoleLogger();
+
+if (!File.Exists(harFilePath))
+{
+    logger.Error($"File {harFilePath} not found!");
+    return;
+}
+
+logger.Log($"File {harFilePath} found!");
+logger.Log("Start reading the file...");
+
+var fileContent = File.ReadAllText(harFilePath);
+
+logger.Log("End reading the file");
+
+logger.Log("Start parsing...");
+
+var root = JObject.Parse(fileContent);
+
+//var entries = root.SelectTokens("log.entries[*]").ToList();
+var resourceTypes = root.SelectTokens("log.entries[*]._resourceType")
+    .Distinct()
+    .ToList();
+
+logger.GreenRaw(String.Format("|{0,12}|", "Found types"));
+logger.GreenRaw("--------------");
+foreach (var resourceType in resourceTypes)
+{
+    logger.GreenRaw(String.Format("|{0,12}|", resourceType));
+}
+
+logger.Log("Will be parsed: \"xhr\" and \"image\"");
+
+var xhrEntries = root.SelectTokens("log.entries[?(@._resourceType == 'xhr')]").ToList();
+var imageEntries = root.SelectTokens("log.entries[?(@._resourceType == 'image')]").ToList();
+
+logger.Log($"xhr items found - {xhrEntries.Count}");
+logger.Log($"image items found - {imageEntries.Count}");
+
+logger.Log("End parsing");
